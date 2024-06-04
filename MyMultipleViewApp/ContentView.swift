39e50b23fec9345b9d2ee014014ed716f5309aa5
noigamegun <76461912+noigamegun@users.noreply.github.com>
2024-06-04 12:41:08 +0700

    Initial Commit

---
+++ MyMultipleViewApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMultipleViewApp
+//
+//  Created by Thapat Auechaikasem on 4/6/67.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyMultipleViewApp/MyMultipleViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMultipleViewAppApp.swift
+//  MyMultipleViewApp
+//
+//  Created by Thapat Auechaikasem on 4/6/67.
+//
+
+import SwiftUI
+
+@main
+struct MyMultipleViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
