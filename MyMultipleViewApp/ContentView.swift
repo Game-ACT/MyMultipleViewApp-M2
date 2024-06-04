1b9d533d419d0037aa0e56cdd1917cb74de45f7f
noigamegun <76461912+noigamegun@users.noreply.github.com>
2024-06-04 13:02:43 +0700

    Added Views And Main Menu Title

--- MyMultipleViewApp/ContentView.swift
+++ MyMultipleViewApp/ContentView.swift
@@ -9,6 +9,67 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        ZStack {
+            NavigationStack {
+                VStack {
+                    NavigationLink(destination: FirstView()) {
+                        Text("First")
+                            .frame(minWidth: 0,maxWidth: .infinity, maxHeight: 70)
+                            .background(.green)
+                            .font(.system(size: 27,design: .rounded))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .padding(.horizontal, 20)
+                    }
+                    
+                    NavigationLink(destination: SecondView()) {
+                        Text("Second")
+                            .frame(minWidth: 0,maxWidth: .infinity, maxHeight: 70)
+                            .background(.green)
+                            .font(.system(size: 27,design: .rounded))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .padding(.horizontal, 20)
+                    }
+                    
+                    NavigationLink(destination: ThirdView()) {
+                        Text("Third")
+                            .frame(minWidth: 0,maxWidth: .infinity, maxHeight: 70)
+                            .background(.green)
+                            .font(.system(size: 27,design: .rounded))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .padding(.horizontal, 20)
+                    }
+                    
+                    NavigationLink(destination: FourthView()) {
+                        Text("Fourth")
+                            .frame(minWidth: 0,maxWidth: .infinity, maxHeight: 70)
+                            .background(.green)
+                            .font(.system(size: 27,design: .rounded))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .padding(.horizontal, 20)
+                    }
+                    
+                    NavigationLink(destination: FifthView()) {
+                        Text("Fifth")
+                            .frame(minWidth: 0,maxWidth: .infinity, maxHeight: 70)
+                            .background(.green)
+                            .font(.system(size: 27,design: .rounded))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .padding(.horizontal, 20)
+                    }
+                }
+                .navigationTitle("Something Here")
+            }
+        }
     }
 }
 
