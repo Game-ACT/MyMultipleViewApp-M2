1a1fa53b201ed703c193cfc226b538d97b183436
noigamegun <76461912+noigamegun@users.noreply.github.com>
2024-06-07 13:19:26 +0700

    Added First Page "Python" in `FirstView.swift`

--- MyMultipleViewApp/FirstView.swift
+++ MyMultipleViewApp/FirstView.swift
@@ -7,9 +7,23 @@
 
 import SwiftUI
 
+var name = "Python"
+var img = "py"
+var imgwidth: CGFloat = 200
+var imgheight: CGFloat = 200
+var about = "Python is a programming language known for its simplicity and readability. It is commonly used for web development, scientific computing, data analysis, artificial intelligence, and more."
+
 struct FirstView: View {
     var body: some View {
-        Text("This is the first view.")
+        Text(name)
+            .font(.system(size: 40, design: .rounded))
+        Image(img)
+            .resizable()
+            .frame(width: imgwidth, height: imgheight)
+        Text(about)
+            .multilineTextAlignment(.center)
+            .font(.system(size: 20))
+            .padding(.horizontal, 30)
     }
 }
 
